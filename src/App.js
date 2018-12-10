ec0c20bd7587283d86a042f6f880a224a61b64c5
Andriy Ozhytchuk <ozhytchuk@gmail.com>
2018-12-10 20:01:43 +0200

    LITS-3: removed paragraph from App.js file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <p>asd</p>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
