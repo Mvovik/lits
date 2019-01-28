c3fd2b509db3be417169b8029b613b9ef3c947d9
Tetiana <44621631+tetianasymotiuk@users.noreply.github.com>
2019-01-28 20:20:09 +0200

    creation Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from './component/Button';
 import List from './component/List';
+import Footer from './component/Footer';
 
 class App extends Component {
   state = {
@@ -27,19 +28,34 @@ class App extends Component {
   render() {
     const { todoList, value } = this.state;
     return (
-      <section className="todo-container">
-        <form onSubmit={this.handleSubmit}>
-          <header>
-            <h1>todos</h1>
-            <input type="text" value={value} onChange={this.onChange} />
-          </header>
-          <section>
-            <List todoList={todoList} />
-          </section>
-        </form>
-      </section>
+      <div>
+        <section className="todo-container">
+          <form onSubmit={this.handleSubmit}>
+            <header>
+              <h1>todos</h1>
+              <input type="text" value={value} onChange={this.onChange} />
+            </header>
+            <section>
+              <List todoList={todoList} />
+            </section>
+          </form>
+        </section>
+        <Footer/>  
+      </div>
     );
   }
 }
+
+
+
+/*<footer class="info">
+<p>Double-click to edit a todo</p>
+<p>Created by <a href="http://github.com/petehunt/">petehunt</a></p>
+<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>*/
+
+
+
+
 
 export default App;
