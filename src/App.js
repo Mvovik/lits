647163e4debe3bf1b54efecee0c345bbb5b4659b
dannydekker161 <dannydekkerbox@egmail.com>
2019-01-28 20:17:15 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from './component/Button';
 import List from './component/List';
+import Footer from './component/Footer';
 
 class App extends Component {
   state = {
@@ -27,19 +28,37 @@ class App extends Component {
   render() {
     const { todoList, value } = this.state;
     return (
-      <section className="todo-container">
-        <form onSubmit={this.handleSubmit}>
-          <header>
-            <h1>todos</h1>
-            <input type="text" value={value} onChange={this.onChange} />
-          </header>
-          <section>
-            <List todoList={todoList} />
-          </section>
-        </form>
-      </section>
+      <div>
+        <section className="todo-container">
+          <form onSubmit={this.handleSubmit}>
+            <header>
+              <h1>todos</h1>
+              <input type="text" value={value} onChange={this.onChange} />
+            </header>
+            <section>
+              <List todoList={todoList} />
+            </section>
+          </form>
+        </section>
+        <Footer/>  
+      </div>
     );
   }
 }
 
+
+
+/*<footer class="info">
+<p>Double-click to edit a todo</p>
+<p>Created by <a href="http://github.com/petehunt/">petehunt</a></p>
+<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>*/
+/*
+<footer class="footer" data-reactid=".0.2"><span class="todo-count" data-reactid=".0.2.0"><strong data-reactid=".0.2.0.0">1</strong><span data-reactid=".0.2.0.1"> </span><span data-reactid=".0.2.0.2">item</span><span data-reactid=".0.2.0.3"> left</span></span><ul class="filters" data-reactid=".0.2.1"><li data-reactid=".0.2.1.0"><a href="#/" class="selected" data-reactid=".0.2.1.0.0">All</a></li><span data-reactid=".0.2.1.1"> </span><li data-reactid=".0.2.1.2"><a href="#/active" class="" data-reactid=".0.2.1.2.0">Active</a></li><span data-reactid=".0.2.1.3"> </span><li data-reactid=".0.2.1.4"><a href="#/completed" class="" data-reactid=".0.2.1.4.0">Completed</a></li></ul></footer>
+*/
+
+
+
 export default App;
+
+
