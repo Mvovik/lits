6c4ed37edbdd07417182c4355fb6e414dc081a69
Mykhailo Svystun <mysvystun@gmail.com>
2019-01-30 20:28:50 +0200

    1

--- src/component/Input/Input.jsx
+++ src/component/Input/Input.jsx
@@ -4,10 +4,18 @@ class Input extends Component {
     render() {
         return (
         <label>
-            <input type="text" value={value} onChange={this.onChange} className="mainInput" autoFocus />
+            <input type="text" value={value} onChange={this.onChange} className="mainInput" {...this.props} autoFocus />
         </label>
         );
     }
 }
+
+Input.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+Input.defaultProps = {
+  onClick: () => ({})
+};
 
 export default Input;
