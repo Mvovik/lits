776cc1cf1144dded7014a4d1825f30e29386ae39
Mvovik <melnyk.vo@gmail.com>
2019-01-30 19:08:47 +0200

    some changes

--- src/component/Input/Input.jsx
+++ src/component/Input/Input.jsx
@@ -1,6 +1,64 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+
+const ENTER_KEY_CODE = 13
+
+class Input extends Component {
+  constructor(props) {
+    super(props)
+    this.onSave = props.onSave
+    this.state = {
+      value: props.value
+    }
+  }
+
+  onChange = event => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  save() {
+    this.onSave(this.state.value)
+    this.setState({
+      value: ''
+    })
+  }
+
+  onKeyDown = event => {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      this.save()
+    }
+  }
+
+  render() {
+    console.log(this.props.value)
+    return (
+      <input
+        type="text"
+        onChange={this.onChange.bind(this)}
+        onKeyDown={this.onKeyDown.bind(this)}
+        onSave={this.save.bind(this)}
+        value={this.state.value} />
+    )
+  }
+}
+
+export default Input
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 class Input extends Component {
   render() {
     return <input type="text" {...this.props} />;
@@ -16,3 +74,4 @@ Input.defaultProps = {
 };
 
 export default Input;
+*/
--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -3,17 +3,33 @@ import PropTypes from 'prop-types';
 import CrossIcon from '../Icons/CrossIcon';
 import Checkbox from '../Checkbox/Checkbox';
 import Button from '../Button';
+import Input from '../Input';
 
 class List extends Component {
+state={
+  showMe: false
+}
   todoListItem = (key, item) => {
     return (
       <li key={key}>
         <Checkbox />
-        <label>{item}</label>
+        {this.onDoubleClick(item)}
         <Button />
       </li>
     );
   };
+
+onClick = event => {
+this.setState({showMe: true})
+}
+
+  onDoubleClick = (item) => {
+    if(this.state.showMe) { 
+       return (<Input value={item}/>);
+    } else { 
+        return (<label onClick={this.onClick}>{item}</label>);
+      } 
+  }
 
   render() {
     const { todoList } = this.props;
